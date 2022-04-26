c96631653bc96e0acec7e33a3a58585e6ab43893
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-04-26 17:27:37 +0600

    customer repository

--- CoreJwtExample/IRepository/ICustomerRepository.cs
+++ CoreJwtExample/IRepository/ICustomerRepository.cs
@@ -4,6 +4,9 @@ namespace CoreJwtExample.IRepository
 {
     public interface ICustomerRepository
     {
+        Task<Customer> Save(Customer obj);
+        Task<Customer> Get(int objId);
+        Task<string> Delete(Customer obj);
         Task<List<Customer>> Gets1();
         Task<List<Customer>> Gets2();
     }
--- CoreJwtExample/Repository/CustomerRepository.cs
+++ CoreJwtExample/Repository/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using CoreJwtExample.IRepository;
+﻿using CoreJwtExample.Common;
+using CoreJwtExample.IRepository;
 using CoreJwtExample.Models;
 using Dapper;
 using System.Data;
@@ -15,6 +16,48 @@ namespace CoreJwtExample.Repository
         public CustomerRepository(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("StoreDb");
+        }
+
+        public async Task<string> Delete(Customer obj)
+        {
+            string message = "";
+            try
+            {
+                using (IDbConnection connection = new SqlConnection(_connectionString))
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    var customer = await connection.QueryAsync<User>("SP_Customer",
+                        this.SetParameters(obj, (int)OperationType.Delete),
+                        commandType: CommandType.StoredProcedure);
+                    message = "Deleted";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return message;
+        }
+
+        public async Task<Customer> Get(int objId)
+        {
+            _customer = new Customer();
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                var customers = await connection.QueryAsync<Customer>(string.Format(@"select * from Customer where CustomerId={0}", objId));
+                if (customers != null && customers.Count() > 0)
+                {
+                    _customer = customers.SingleOrDefault();
+                }
+                return _customer;
+            }
         }
 
         public async Task<List<Customer>> Gets1()
@@ -51,6 +94,47 @@ namespace CoreJwtExample.Repository
                 }
                 return _customersList;
             }
+        }
+
+        public async Task<Customer> Save(Customer obj)
+        {
+            _customer = new Customer();
+            try
+            {
+                int operationType = Convert.ToInt32(obj.CustomerId == 0 ? OperationType.Insert : OperationType.Update);
+
+                using (IDbConnection connection = new SqlConnection(_connectionString))
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    var customers = await connection.QueryAsync<Customer>("SP_Customer",
+                         this.SetParameters(obj, operationType),
+                         commandType: CommandType.StoredProcedure);
+                    if (customers != null && customers.Count() > 0)
+                    {
+                        _customer = customers.FirstOrDefault();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _customer = new Customer();
+                _customer.Message = ex.Message;
+            }
+            return _customer;
+        }
+
+        private DynamicParameters SetParameters(Customer _customer, int _operationType)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@CustomerId", _customer.CustomerId);
+            parameters.Add("@CustomerName", _customer.CustomerName);
+            parameters.Add("@Role", _customer.Role);
+            parameters.Add("@OperationType", _operationType);
+            return parameters;
+
         }
     }
 }
